f1af40b799560860b37dc3a82460773f02c15367
Samantha <samanthabarrueta@gmail.com>
2020-04-21 11:03:28 -0500

    make var numbers a string
    listed as individual numbers

--- Develop/script.js
+++ Develop/script.js
@@ -18,7 +18,8 @@ generateBtn.addEventListener("click", writePassword);
 var lowerCase = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z'
 var upperCase = 'A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z';
 var specChars = ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
-var numbers = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9;
+var numbers = '0, 1, 2, 3, 4, 5, 6, 7, 8, 9';
 var userSelects = '';
 var password = '';
+
 
