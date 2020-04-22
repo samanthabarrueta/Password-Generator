b83174e20a75fd8f5418e8e97e6af93a563c4648
Samantha <samanthabarrueta@gmail.com>
2020-04-22 00:03:28 -0500

    continuing

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,20 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+function userLength(passwordLength) {
+  var passwordLength = 0;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  while (passwordLength < 8 || passwordLength > 128) {
+    var passwordLength = prompt('How many characters would you like to include? Choose between 8-128');
+  
+  return passwordLength;
+  } 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// start of my code
-var lowerCase = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z'
-var upperCase = 'A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z';
-var specChars = ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
-var numbers = '0, 1, 2, 3, 4, 5, 6, 7, 8, 9';
-var userSelects = '';
-var password = '';
-
-function userOptions(charsIncluded) {
-  var passwordLength = 0;
-  while (passwordLength < 8 || passwordLength > 128) {
-   var passwordLength = prompt('How many characters would you like to include? Choose between 8-128');
- 
-
+function userOptions(userSelects) {
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCase = ["A", "B", "C", "D", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var specChars = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+  //var userSelects = [];
+    
   var hasNumbers = confirm('Would you like to include numbers 0-9?');  
   var hasLowers = confirm('Would you like to include lowercase letters?'); 
   var hasUppers = confirm('Would you like to include uppercase letters?');
@@ -48,7 +35,27 @@ function userOptions(charsIncluded) {
   if (hasChars === true) {
     userSelects.push(specChars);
     alert('You have included special characters');
+
+  return userSelects;
   }
 }
 
-userOptions();
+function finalSelection(userSelects, passwordLength, myPassword) {
+  var myPassword = [];
+
+  for (var i = 0; i < passwordLength; i++) {
+    myPassword = userSelects[Math.floor(Math.random() * userSelects.length)];
+  
+  return myPassword; 
+  }
+}
+
+function generatePassword() {
+  userLength();
+  userOptions();
+  finalSelection();   
+  return password; 
+}
+
+
+
